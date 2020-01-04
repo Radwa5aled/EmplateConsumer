2e4576c9928f5b78ed42796411657a2c9a094b24
Radwa Khaled <radwa.khaled@appsinnovate.com>
2020-01-04 12:09:59 +0200

    first push

---
+++ EmplateConsumer/EmplateConsumer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmplateConsumer
+//
+//  Created by Radwa Khaled on 1/4/20.
+//  Copyright © 2020 Radwa Khaled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
