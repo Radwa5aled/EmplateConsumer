251556d596478e7f677dbb68c0a0b789123b8faa
Radwa Khaled <radwa.khaled@appsinnovate.com>
2020-01-04 12:23:12 +0200

    add fonts and constants

---
+++ EmplateConsumer/EmplateConsumer/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  EmplateConsumer
+//
+//  Created by Radwa Khaled on 1/4/20.
+//  Copyright © 2020 Radwa Khaled. All rights reserved.
+//
+
+import Foundation
+
+class Constants: NSObject {
+    
+    static let baseURL = "https://emplate.dev/v9/"
+    static let apiKey = "$2y$10$gpin5yXpMxbjWvsnnDMhLOYO7kpZSag7CKIDndWmUeJ/4WEchaAxK"
+    static let postsPath = "posts"
+    
+    
+}
